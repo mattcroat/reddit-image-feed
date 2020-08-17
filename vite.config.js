bf80398a7839f2642b96b90247139d4b50347823
mattcroat <mattcroat.dev@gmail.com>
2020-08-17 19:40:03 +0200

    Add path aliases

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 // @ts-check
 const preactRefresh = require('@prefresh/vite');
+const path = require('path');
 
 /**
  * @type { import('vite').UserConfig }
@@ -7,6 +8,12 @@ const preactRefresh = require('@prefresh/vite');
 const config = {
   jsx: 'preact',
   plugins: [preactRefresh()],
+  alias: {
+    '/@components/': path.resolve(__dirname, './src/components'),
+    '/@css/': path.resolve(__dirname, './src/css'),
+    '/@hooks/': path.resolve(__dirname, './src/hooks'),
+    '/@utility/': path.resolve(__dirname, './src/utility'),
+  },
 };
 
 module.exports = config;
