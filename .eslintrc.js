0f0ac9499894c82ca2c174fe821d0086ebcd5e2e
mattcroat <mattcroat.dev@gmail.com>
2020-08-18 22:52:19 +0200

    Update ESLint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,7 @@ module.exports = {
     browser: true,
     es2020: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ['plugin:react/recommended', 'airbnb'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -14,9 +11,17 @@ module.exports = {
     ecmaVersion: 11,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react'],
   rules: {
+    'react/react-in-jsx-scope': 'off',
+    'import/no-unresolved': 'off',
+    'import/no-absolute-path': 'off',
+    'import/extensions': 'off',
+    'react/no-unknown-property': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'no-console': 'off',
+    'consistent-return': 'off',
+    'no-restricted-globals': 'off',
   },
 };
