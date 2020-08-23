7e4271d01e629a660349b497cae160fbb377504b
mattcroat <mattcroat.dev@gmail.com>
2020-08-23 19:41:12 +0200

    Refactor Loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,6 @@
 const Loader = () => (
-  <div class="h-screen flex justify-center">
-    <img
-      class="h-1/4 w-1/4"
-      src="http://samherbert.net/svg-loaders/svg-loaders/puff.svg"
-      alt=""
-    />
+  <div class="flex justify-center">
+    <img src="http://samherbert.net/svg-loaders/svg-loaders/oval.svg" alt="" />
   </div>
 );
 
